5003c8ec715414fc65144c33a2a419ae4cc9c8c6
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2025-05-13 00:28:52 +0800

    block

---
+++ src/core/block/block.rs
@@ -0,0 +1,24 @@
+use borsh_derive::{BorshDeserialize, BorshSerialize};
+
+#[derive(Debug, Clone, BorshSerialize, BorshDeserialize)]
+pub struct Header {
+    pub chain_id: u64,
+    pub height: u64,
+    pub time: u64,
+
+    pub header_hash_prev: [u8; 32],
+
+    pub txs_root: [u8; 32],
+
+    pub state_root: [u8; 32],
+    pub keys_root: [u8; 32],
+
+    pub sequencer_hash: [u8; 32],
+    pub sequencer_hash_next: [u8; 32],
+}
+
+pub struct Block {
+    pub header: Header,
+    pub txs: Vec<Vec<u8>>,
+    pub sequencer_signature: Vec<u8>,
+}
--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,9 +1,11 @@
 mod app;
+mod block;
 mod context;
 mod module;
 mod tx;
 
 pub use app::*;
+pub use block::*;
 pub use context::*;
 pub use module::*;
 pub use tx::*;
