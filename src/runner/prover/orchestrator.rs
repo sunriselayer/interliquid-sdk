77a05c8f2be9fb265503fba802b7cb1dd621f548
KIMURA Yu <33382781+KimuraYu45z@users.noreply.github.com>
2025-05-17 18:23:34 +0800

    noop

--- src/runner/prover/orchestrator.rs
+++ src/runner/prover/orchestrator.rs
@@ -6,20 +6,23 @@ use super::ProverInstance;
 
 pub struct ProverOrchestrator {
     instances: Vec<Box<dyn ProverInstance>>,
-    next_instance: usize,
+    next_instance: Option<usize>,
     sender: Sender<RunnerMessage>,
     receiver: Receiver<RunnerMessage>,
 }
 
 impl ProverOrchestrator {
+    /// `instances` can be emtpy, in which case the prover should be no-op, for example optimistic sovereign rollup.
     pub fn new(
         instances: Vec<Box<dyn ProverInstance>>,
         sender: Sender<RunnerMessage>,
         receiver: Receiver<RunnerMessage>,
     ) -> Self {
+        let next_instance = if instances.is_empty() { None } else { Some(0) };
+
         Self {
             instances,
-            next_instance: 0,
+            next_instance,
             sender,
             receiver,
         }
@@ -27,12 +30,14 @@ impl ProverOrchestrator {
 
     pub async fn run(&mut self) -> Result<(), InterLiquidSdkError> {
         while let Ok(msg) = self.receiver.recv().await {
-            match msg {
-                RunnerMessage::TxProofReady(msg) => {}
-                RunnerMessage::TxProofAggregationReady(msg) => {}
-                RunnerMessage::CommitStateProofReady(msg) => {}
-                RunnerMessage::CommitKeysProofReady(msg) => {}
-                _ => {}
+            if let Some(next_instance) = self.next_instance {
+                match msg {
+                    RunnerMessage::TxProofReady(msg) => {}
+                    RunnerMessage::TxProofAggregationReady(msg) => {}
+                    RunnerMessage::CommitStateProofReady(msg) => {}
+                    RunnerMessage::CommitKeysProofReady(msg) => {}
+                    _ => {}
+                }
             }
         }
 
